5cdb2ce86f349fd24a31dc397588689e33719cb0
Joshua T Corbin <jcorbin@wunjo.org>
2024-05-09 00:08:15 -0400

    Use --all for gitvar describe

--- git_scad_vars.sh
+++ git_scad_vars.sh
@@ -4,7 +4,8 @@ set -e
 case "$1" in
 
   smudge)
-    desc=$(git describe --always --abbrev)
+    desc=$(git describe --always --all --abbrev)
+    desc=${desc#*/}
     sed -r \
       -e 's/".*";( *\/\/ *\$gitvar\$describe\$)/"'"$desc"'";\1/'
     ;;
