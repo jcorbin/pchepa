9c0b2d870bf90bf133b9092dffec5777b1b897c6
Joshua T Corbin <jcorbin@wunjo.org>
2024-05-09 08:49:17 -0400

    Refactor release script
    - fix idiomatic git-cim alias
    - break main flow up into routines
    - change to always edit for rc
    - further reduce regen parallelism

--- release.sh
+++ release.sh
@@ -5,11 +5,15 @@ set -x
 desc=$(git describe --tags)
 last_version=${desc%%-*}
 
-if ! next_version=$(sed -e "/^# ${last_version}/q" <CHANGELOG.md | head -n-1 | grep -om1 '# v..*' | grep -o 'v.*'); then
+get_next_version() {
+  sed -e "/^# ${last_version}/q" <CHANGELOG.md \
+    | head -n-1 | grep -om1 '# v..*' | grep -o 'v.*'
+}
 
+regen_changelog() {
   while read -r line; do
-    if [[ $line =~ "$last_version" ]]; then
-      echo "# vNEXT"
+    if [[ $line =~ ^##*\ \ #v ]]; then
+      echo "# ${next_version:-vNEXT}"
       echo
       echo '```git log --pretty=oneline'
       git log "$last_version".. --pretty=oneline
@@ -17,16 +21,29 @@ if ! next_version=$(sed -e "/^# ${last_version}/q" <CHANGELOG.md | head -n-1 | g
       echo
     fi
     echo "$line"
-  done <CHANGELOG.md >newch
+  done <CHANGELOG.md
+}
+
+edit_changelog() {
+  regen_changelog >newch
   mv newch CHANGELOG.md
   $EDITOR CHANGELOG.md
+  next_version=$(get_next_version)
+  if ! git diff --quiet CHANGELOG.md; then
+    git add CHANGELOG.md
+    git commit -m "Update changelog for $next_version"
+  fi
+}
 
-  next_version=$(sed -e "/^# ${last_version}/q" <CHANGELOG.md | head -n-1 | grep -om1 '# v..*' | grep -o 'v.*')
-  git add CHANGELOG.md
-  git cim "Update changelog for $next_version"
+prep_next_version() {
+  [ -n "$next_version" ]
+  git tag -f "$next_version"
+  make -j2 regen
+  git tag -f "$next_version"
+}
 
-fi
+edit_changelog
+prep_next_version
 
-git tag "$next_version"
-make -j8 regen
-git tag -a -f "$next_version"
+# TODO cut release by tag promotion
+# git tag -a -f "$next_version" $next_version
